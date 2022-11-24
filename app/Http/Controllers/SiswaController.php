d3a4528925a44499facfe8d24b43b8cadf504701
Ghaluh Kusuma <ghaluhkusuma21@gmail.com>
2022-11-24 08:40:17 +0700

    Make Eloquent, Blade, Paginator

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\siswa;
 use Illuminate\Http\Request;
+
 
 class SiswaController extends Controller
 {
     //
     function index(){
-        return "<h1>Saya SISWA dari Controller</h1>";
+        // $data = siswa :: all();
+        $data = siswa::orderBy("nomor_induk","desc")->paginate(1);
+        return view("siswa/index")->with("data",$data);
     }
     function detail($id){
-        return "<h1>Saya SISWA dari Controller dengan ID $id </h1>";
+      //  return "<h1>Saya SISWA dari Controller dengan ID $id </h1>";
+    $data = siswa::where("nomor_induk", $id)->first();
+    return view("siswa/show")->with("data", $data);
     }
 }
